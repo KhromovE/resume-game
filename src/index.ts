691429058847ed87ab190f4bfe3948fd96f4e082
Egor <KhromovE@gmail.com>
2019-12-08 18:42:36 +0700

    add full screen

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,16 @@
-import { Application } from 'pixi.js'
+import * as PIXI from 'pixi.js'
 
-const app = new Application({
+const app = new PIXI.Application({
     width: 256,
     height: 256,
     antialias: true,
     transparent: false,
     resolution: 1,
     backgroundColor: 0x061639,
+    autoDensity: true,
 })
+app.renderer.view.style.position = 'absolute'
+app.renderer.view.style.display = 'block'
+app.renderer.resize(window.innerWidth, window.innerHeight)
 
 document.body.appendChild(app.view)
