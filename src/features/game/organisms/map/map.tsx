c37af3eb3c1d4c2a506ff272dc7644a0c5fd5731
Egor <khromove@gmail.com>
2020-02-07 21:26:28 +0700

    first try

--- src/features/game/organisms/map/map.tsx
+++ src/features/game/organisms/map/map.tsx
@@ -1,18 +1,14 @@
 import * as React from 'react'
-import { Container, useApp, Sprite } from '@inlet/react-pixi'
+import { Container, Sprite } from '@inlet/react-pixi'
 import { Point } from 'pixi.js'
+
+import tvSprite from '../../../../assets/tv.svg'
 
 const obsp = new Point(1, 0.5)
 const spriteScale = new Point(1, 2)
 
-export const Map: React.SFC = () => {
-  const {
-    screen: { width, height },
-  } = useApp()
-
-  return (
-    <Container scale={obsp} position={[width / 2, height / 2]}>
-      <Sprite image="https://pixijs.io/examples/examples/assets/eggHead.png" scale={spriteScale} />
-    </Container>
-  )
-}
+export const Map: React.SFC = () => (
+  <Container scale={obsp}>
+    <Sprite image={tvSprite} scale={spriteScale} />
+  </Container>
+)
