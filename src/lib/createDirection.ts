11b0cb85d669bc003810856d9624eefd38bb6fa9
Egor <khromove@gmail.com>
2020-02-27 21:50:09 +0700

    movement without spam

--- src/lib/createDirection.ts
+++ src/lib/createDirection.ts
@@ -1,59 +1,60 @@
-import { createStore, createEvent, Store } from 'effector'
+import { createStore, createEvent, guard, Store, sample } from 'effector'
 import * as R from 'ramda'
 
-type Left = {
+type Direction = {
+  up: boolean
+  down: boolean
   left: boolean
-}
-
-type Right = {
   right: boolean
 }
 
-type Down = {
-  down: boolean
-}
+type SingleDirection = [keyof Direction, boolean]
 
-type Up = {
-  up: boolean
-}
-
-type Direction = Left & Right & Down & Up
-
-type SingleDirection = Left | Right | Down | Up
-
-type Returned = {
+type Result = {
   $direction: Store<Direction>
 }
 
-export const createDirection = (): Returned => {
-  const updateDirection = createEvent<SingleDirection>()
-  const buttonPressed = R.compose(
-    updateDirection,
-    R.cond<number, SingleDirection>([
-      [R.equals(37), R.always({ left: true })],
-      [R.equals(39), R.always({ right: true })],
-      [R.equals(40), R.always({ down: true })],
-      [R.equals(38), R.always({ up: true })],
-    ]),
-    R.prop<'keyCode', number>('keyCode'),
-  )
+const UP = 87
+const DOWN = 83
+const LEFT = 65
+const RIGHT = 68
+const takeKeyCode = R.prop<'keyCode', number>('keyCode')
+const compareDircetionWithPart = ([direction, part]: [Direction, SingleDirection]): boolean => {
+  const [partKey, partValue] = part
+  const dirValue = R.prop(partKey, direction)
 
-  const buttonReleased = R.compose(
-    updateDirection,
-    R.cond<number, SingleDirection>([
-      [R.equals(37), R.always({ left: false })],
-      [R.equals(39), R.always({ right: false })],
-      [R.equals(40), R.always({ down: false })],
-      [R.equals(38), R.always({ up: false })],
-    ]),
-    R.prop<'keyCode', number>('keyCode'),
-  )
+  return R.not(R.equals(dirValue, partValue))
+}
+
+const prepareDirection = (status: boolean): ((n: number) => SingleDirection) =>
+  R.cond<number, SingleDirection>([
+    [R.equals(UP), R.always(['up', status])],
+    [R.equals(DOWN), R.always(['down', status])],
+    [R.equals(LEFT), R.always(['left', status])],
+    [R.equals(RIGHT), R.always(['right', status])],
+  ])
+
+const move = R.compose(prepareDirection(true), takeKeyCode)
+const stop = R.compose(prepareDirection(false), takeKeyCode)
+
+export const createDirection = (): Result => {
+  const updateDirection = createEvent<[Direction, SingleDirection]>()
+  const buttonEvent = createEvent<SingleDirection>()
+  const buttonPressed = R.compose(buttonEvent, move)
+  const buttonReleased = R.compose(buttonEvent, stop)
+
   const $direction = createStore<Direction>({
     up: false,
-    right: false,
     down: false,
     left: false,
-  }).on<SingleDirection>(updateDirection, (state, payload) => R.mergeRight(state, payload))
+    right: false,
+  }).on(updateDirection, (_, [state, [key, value]]) => R.mergeRight(state, { [key]: value }))
+
+  guard<[Direction, SingleDirection]>({
+    source: sample($direction, buttonEvent, (dir, part) => [dir, part]),
+    filter: compareDircetionWithPart,
+    target: updateDirection,
+  })
 
   document.addEventListener('keydown', buttonPressed)
   document.addEventListener('keyup', buttonReleased)
