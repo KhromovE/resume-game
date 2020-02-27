74fd515d53e2ca7aeb6160ecf19cdb4a0c2e007b
Egor <khromove@gmail.com>
2020-02-28 00:20:37 +0700

    better approach

--- src/lib/createDirection.ts
+++ src/lib/createDirection.ts
@@ -1,14 +1,8 @@
 import { createStore, createEvent, guard, Store, sample } from 'effector'
 import * as R from 'ramda'
 
-type Direction = {
-  up: boolean
-  down: boolean
-  left: boolean
-  right: boolean
-}
-
-type SingleDirection = [keyof Direction, boolean]
+type SingleDirection = 'up' | 'down' | 'left' | 'right'
+type Direction = SingleDirection[]
 
 type Result = {
   $direction: Store<Direction>
@@ -23,42 +17,36 @@ const compareDircetionWithPart = ([direction, part]: [
   Direction,
   SingleDirection | undefined,
 ]): boolean => {
-  if (part === undefined) return false
+  if (R.isNil(part)) return false
 
-  const [partKey, partValue] = part
-  const dirValue = R.prop(partKey, direction)
-
-  return R.not(R.equals(dirValue, partValue))
+  return R.not(R.contains(part, direction))
 }
 
-const prepareDirection = (status: boolean): ((n: number) => SingleDirection) =>
-  R.cond<number, SingleDirection>([
-    [R.equals(UP), R.always(['up', status])],
-    [R.equals(DOWN), R.always(['down', status])],
-    [R.equals(LEFT), R.always(['left', status])],
-    [R.equals(RIGHT), R.always(['right', status])],
-  ])
+const checkDirection = R.cond<number, SingleDirection>([
+  [R.equals(UP), R.always('up')],
+  [R.equals(DOWN), R.always('down')],
+  [R.equals(LEFT), R.always('left')],
+  [R.equals(RIGHT), R.always('right')],
+])
 
-const move = R.compose(prepareDirection(true), takeKeyCode)
-const stop = R.compose(prepareDirection(false), takeKeyCode)
+const move = R.compose(checkDirection, takeKeyCode)
+const stop = R.compose(checkDirection, takeKeyCode)
 
 export const createDirection = (): Result => {
-  const updateDirection = createEvent<[Direction, SingleDirection]>()
-  const buttonEvent = createEvent<SingleDirection>()
-  const buttonPressed = R.compose(buttonEvent, move)
-  const buttonReleased = R.compose(buttonEvent, stop)
+  const addDirection = createEvent<[Direction, SingleDirection]>()
+  const removeDirection = createEvent<SingleDirection>()
+  const tryDirection = createEvent<SingleDirection>()
+  const buttonPressed = R.compose(tryDirection, move)
+  const buttonReleased = R.compose(removeDirection, stop)
 
-  const $direction = createStore<Direction>({
-    up: false,
-    down: false,
-    left: false,
-    right: false,
-  }).on(updateDirection, (_, [state, [key, value]]) => R.mergeRight(state, { [key]: value }))
+  const $direction = createStore<Direction>([])
+    .on(addDirection, (state, [, part]) => R.concat(state, [part]))
+    .on(removeDirection, (state, part) => R.without([part], state))
 
   guard<[Direction, SingleDirection]>({
-    source: sample($direction, buttonEvent, (dir, part) => [dir, part]),
+    source: sample($direction, tryDirection, (dir, part) => [dir, part]),
     filter: compareDircetionWithPart,
-    target: updateDirection,
+    target: addDirection,
   })
 
   document.addEventListener('keydown', buttonPressed)
